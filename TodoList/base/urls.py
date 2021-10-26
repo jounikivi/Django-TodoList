72906c0456bdd0d547d77f076c834768161eeebe
Kiviperä Jouni <jouni.kivipera@turku.fi>
2021-10-26 13:02:07 +0300

    lisäyksiä

--- TodoList/base/urls.py
+++ TodoList/base/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .import views
+#from .import views
+from .views import *
+
 
 urlpatterns = [
-    path('', views.taskList, name='tasks')
+    path('', TaskList.as_view(), name='tasks'),
 ]
